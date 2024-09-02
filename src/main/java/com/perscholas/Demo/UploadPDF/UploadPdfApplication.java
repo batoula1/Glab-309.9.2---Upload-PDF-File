af71acae4a113fe0c68ddef208ac27d16cfcedad
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-02 00:25:36 -0400

    PDF

---
+++ src/main/java/com/perscholas/Demo/UploadPDF/UploadPdfApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.Demo.UploadPDF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadPdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/Demo/UploadPDF/UploadPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.Demo.UploadPDF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
